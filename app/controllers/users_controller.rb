28c3cc677479261d22a33132c07ffe5e201098b2
awaiszafar14 <awais.zafar@whizzbridge.com>
2023-07-21 16:16:08 +0000

    Pundit still to be used

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+    skip_before_action :authenticate_user!, only: [:index]
+    #before_action :require_same_user, except: [:index]
+
+
     def index
         @users = User.all
     end
@@ -14,6 +18,14 @@ class UsersController < ApplicationController
         redirect_to users_path
     end
 
+    private
+
+        def require_same_user
+            if current_user != @user
+                flash[:alert] = "You can edit or delete your profile"
+                redirect_to users_path
+            end
+        end
 
 
 end
